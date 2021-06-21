c31f1c77919efad3f06fca100e26acc459f5da35
Felipe Hoffa <felipe.hoffa@snowflake.com>
2021-06-21 12:49:43 -0700

    Create stations_city.sql

---
+++ knoema_noaa_dbt/models/stations_city.sql
@@ -0,0 +1,10 @@
+{{config(materialized='table')}}
+
+select label city, station, st_distance(st_makepoint(a.lon, a.lat), st_makepoint(b.lon, b.lat)) distance, country_fips, b.country
+    , station_id
+from {{ ref('weather_stations') }} a
+join {{ ref('wikidata_large_cities') }} b
+on a.country=b.country_fips
+and st_distance(st_makepoint(a.lon, a.lat), st_makepoint(b.lon, b.lat)) < 50000
+qualify row_number() over(partition by station order by distance) = 1
+order by views desc
